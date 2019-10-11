530cf5d1eaa2a7e8dcebe5013116a04e50321ecf
montanarice <davidrice363@gmail.com>
2019-10-11 13:11:19 -0600

    Roslynator SCS updates: analyze(), working output, scratch test

--- src/main/java/qatch/csharp/Roslynator.java
+++ src/main/java/qatch/csharp/Roslynator.java
@@ -4,6 +4,8 @@ import qatch.analysis.AnalylsisResult;
 import qatch.analysis.ITool;
 import qatch.analysis.Tool;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 /**
@@ -20,14 +22,51 @@ import java.nio.file.Path;
 public class Roslynator extends Tool implements ITool {
 
     private Path toolsDirectory;
+    private Path msBuild;
 
-    public Roslynator(Path toolsDirectory) {
+    public Roslynator(Path toolsDirectory, Path msBuild) {
         this.setName("Roslynator");
         this.toolsDirectory = toolsDirectory;
+        this.msBuild = msBuild;
     }
 
     @Override
     public Path analyze(Path path) {
+
+        String sep = File.separator;
+        ProcessBuilder pb;
+        String tempResults = "C:\\Users\\davidrice3\\Desktop\\temp\\out\\roslynator_output.xml";
+
+        // strings for CLI call
+        String roslynator = toolsDirectory.toAbsolutePath().toString() + sep + "Roslynator" + sep + "bin" + sep + "Roslynator.exe";
+        String command = "analyze";
+        String assemblyDir = "--analyzer-assemblies=" + toolsDirectory.toString() + sep + "Roslynator" + sep + "SecurityCodeScan";
+        String msBuild = "--msbuild-path=" + this.msBuild.toString();
+        String output = "--output=" + tempResults;
+        String target = path.toString();
+
+        if(System.getProperty("os.name").contains("Windows")){
+            pb = new ProcessBuilder("cmd.exe", "/c", roslynator, command, assemblyDir, msBuild, output, target);
+        } else {
+            throw new RuntimeException("Roslynator C# analysis not supported on non-Windows machines.");
+        }
+
+        pb.redirectErrorStream(true);
+        Process p = null;
+
+        // run the tool
+        try {
+            p = pb.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            assert p != null;
+            p.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
